e3db69c8593a5cd5d3d80d0117bb7127c379cebc
yangkangkai <452306233@qq.com>
2018-07-04 22:30:18 +0800

    20180704:2230

--- request_a_t.js
+++ request_a_t.js
@@ -1,7 +1,7 @@
 var request = require("request")
 
-var APPID = wx39478853ed9d0e99
-var APPSECRET = be42c16d014d47585dfdb77f29cc6a8c
+var APPID = 'wx39478853ed9d0e99'
+var APPSECRET = 'be42c16d014d47585dfdb77f29cc6a8c'
 
 request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET", function(error, response, body){
     console.log(body)
