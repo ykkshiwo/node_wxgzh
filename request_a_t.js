dd0faa7207a8775e8553255e0e514a3ef069dcdb
yangkangkai <452306233@qq.com>
2018-07-05 10:56:35 +0800

    20180705

--- request_a_t.js
+++ request_a_t.js
@@ -1,8 +1,5 @@
 var request = require("request")
 
-var APPID = 'wx39478853ed9d0e99'
-var APPSECRET = 'be42c16d014d47585dfdb77f29cc6a8c'
-
-request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET", function(error, response, body){
+request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx39478853ed9d0e99&secret=be42c16d014d47585dfdb77f29cc6a8c", function(error, response, body){
     console.log(body)
 })
